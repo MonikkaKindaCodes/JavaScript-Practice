1d6adcb31e0354e82ab3e2a5df18aca61f94d8e7
Monikka Edgeston <59119237+MonikkaKindaCodes@users.noreply.github.com>
2020-09-30 20:48:15 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,2 +1,4 @@
 var name = prompt ("What is your name");
 alert("Hello " + name + " nice to meet you. :)");
+var age = prompt ("How old are you?"); 
+alert("On Janurary 1st 2030, you will be " + 27+10 + " years old :(");
