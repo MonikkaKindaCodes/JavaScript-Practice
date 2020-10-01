e765be15a16eb1dee074872bdbe63dc64f966b35
Monikka Edgeston <59119237+MonikkaKindaCodes@users.noreply.github.com>
2020-09-30 20:52:24 -0500

    Add files via upload

--- JSStalkerExercise.js
+++ JSStalkerExercise.js
@@ -1,5 +1,5 @@
 var firstName = prompt ("What is your first name?"); 
 var lastName = prompt ("What is your last name?"); 
 var age = prompt ("How old are you?");
-alert("Your full name is "+ firstName + " " + lastName + ".");
-alert("You are " + age + " years old.");
+console.log("Your full name is "+ firstName + " " + lastName + ".");
+console.log("You are " + age + " years old.");
