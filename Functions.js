c41bd8fc0fb4720443082a60770794068cd8eb26
Monikka Edgeston <59119237+MonikkaKindaCodes@users.noreply.github.com>
2020-10-03 18:19:52 -0500

    Add files via upload

--- Functions.js
+++ Functions.js
@@ -5,6 +5,19 @@ function isEven(x) {
     return false;
   }
 }
-function factorial(x) {
-  return x;
+function factorial(num) {
+  var result = num;
+  if (num === 0 || num === 1) {
+    return 1;
+  }
+  while (num > 1) {
+    num--;
+    result *= num;
+  }
+  return result;
+}
+function kebabToSnake(myStr) {
+  var myStr = string;
+  myStr = "-";
+  return myStr.replace("-", "_");
 }
