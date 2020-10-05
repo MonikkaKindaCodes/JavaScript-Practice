9a1ef663b0c0394bca233071f32dd9629891fc3e
Monikka Edgeston <59119237+MonikkaKindaCodes@users.noreply.github.com>
2020-10-05 11:04:34 -0500

    Add files via upload

--- toDoList.js
+++ toDoList.js
@@ -3,12 +3,19 @@ var input = prompt("What would you like to do?");
 
 while (input !== "quit") {
     if (input === "list") {
-        toDos.forEach(function (todo) {
-            console.log(todo);
+        console.log("**********");
+        toDos.forEach(function (todo, i) {
+            console.log(i + ": " + todo);
         });
+        console.log("**********");
     } else if (input === "new") {
         var newToDo = prompt("What task would you like to add?");
         toDos.push(newToDo);
+        console.log("Added To Do");
+    } else if (input === "delete") {
+        var index = prompt("Enter index of todo to delete");
+        toDos.splice(index, 1);
+        console.log("Deleted To Do");
     }
     var input = prompt("What would you like to do?");
 }
