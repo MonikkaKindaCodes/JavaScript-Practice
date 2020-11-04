94189a2de4d773e35b0496040482fec556e9edf6
Monikka Edgeston <59119237+MonikkaKindaCodes@users.noreply.github.com>
2020-11-03 20:47:32 -0600

    Add files via upload

--- BMI.js
+++ BMI.js
@@ -1,15 +1,15 @@
-const markFirstHeight = 1.69; 
-const markFirstWeight = 78; 
-const johnFirstHeight = 1.95; 
-const johnFirstWeight = 92;
+// const markFirstHeight = 1.69; 
+// const markFirstWeight = 78; 
+// const johnFirstHeight = 1.95; 
+// const johnFirstWeight = 92;
 
-const markFirstBMI = (markFirstWeight/(markFirstHeight ** 2));
-const johnFirstBMI = (johnFirstWeight/(johnFirstHeight ** 2));
-console.log(markFirstBMI);
-console.log(johnFirstBMI);
+// const markFirstBMI = (markFirstWeight/(markFirstHeight ** 2));
+// const johnFirstBMI = (johnFirstWeight/(johnFirstHeight ** 2));
+// console.log(markFirstBMI);
+// console.log(johnFirstBMI);
 
-const markHigherBMI = markFirstBMI > johnFirstBMI;
-console.log(markHigherBMI);
+// const markHigherBMI = markFirstBMI > johnFirstBMI;
+// console.log(markHigherBMI);
 
 const markSecondHeight = 1.88; 
 const markSecondWeight = 95; 
@@ -19,9 +19,13 @@ const jonSecondWeight = 85;
 const markSecondBMI = (markSecondWeight/(markSecondHeight * markSecondHeight));
 const johnSecondBMI = (jonSecondWeight/(jonSecondHeight * jonSecondHeight));
 
-console.log(markSecondBMI);
-console.log(johnSecondBMI);
+if(markSecondBMI > johnSecondBMI) {
+    console.log(`Mark's BMI (${markSecondBMI.toFixed(2)}) is higher than John's BMI (${johnSecondBMI.toFixed(2)}).`);
+} else {
+    console.log(`John's BMI (${johnSecondBMI.toFixed(2)}) is higher than Mark's BMI (${markSecondBMI.toFixed(2)}).`);
+}
 
-markSecondHighBMI = markSecondBMI > johnSecondBMI;
-console.log(markSecondHighBMI);
+
+
+
 
