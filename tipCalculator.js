87a2a2c9d58d1e248177215353877d4df46abe4d
Monikka Edgeston <59119237+MonikkaKindaCodes@users.noreply.github.com>
2020-11-05 08:26:10 -0600

    Add files via upload

---
+++ tipCalculator.js
@@ -0,0 +1,7 @@
+//Tip Calculator Exercise
+
+const bill = 275;
+let tip =.15 * bill;
+total = tip + bill
+bill >= 20 && bill <= 300 ? tip : tip = .2 * bill, total = tip + bill;
+console.log(`Your bill was ${bill}, the tip was ${tip}, and the total value is ${total}`);
