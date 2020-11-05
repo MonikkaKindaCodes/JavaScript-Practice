1951052ded1e1ce59d9038959d75f10f2d6ab312
Monikka Edgeston <59119237+MonikkaKindaCodes@users.noreply.github.com>
2020-11-05 10:25:33 -0600

    Add files via upload

--- functionExpressions.js
+++ functionExpressions.js
@@ -1,3 +1,5 @@
+
+//Function Expressions can be declared before they are defined
 function calcAge1(birthYear) { //Function Expressions have a name
     return 2020 - birthYear; //You can call the function using the function name
 }
@@ -5,7 +7,7 @@ function calcAge1(birthYear) { //Function Expressions have a name
 const age1 = calcAge1(1993);
 console.log(`I am ${age1}`); // Function Declaration
 
-//Function Expression
+//Function Expression - cannot be declared before they are defined
 //varialble will be the function
  const calcAge2 = function (birthYear) { //Function doesn't have a name/ Anonymous function
     return 2020 - birthYear;            //This is an expression => Expressions have value
@@ -13,3 +15,4 @@ console.log(`I am ${age1}`); // Function Declaration
 
 const age2 = calcAge2(1993);
 console.log(age2, age1);
+
