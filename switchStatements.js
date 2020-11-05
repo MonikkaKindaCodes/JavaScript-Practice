a618b442b7013827a158bf982017c6ea26bf3dec
Monikka Edgeston <59119237+MonikkaKindaCodes@users.noreply.github.com>
2020-11-04 20:54:37 -0600

    Add files via upload

--- switchStatements.js
+++ switchStatements.js
@@ -1,4 +1,4 @@
-const day = 'wednesday';
+// const day = 'sunday';
 
 // switch(day) {
 //     case 'monday'://strict comparison day === monday
@@ -22,18 +22,18 @@ const day = 'wednesday';
 //     default:
 //         console.log(`Not a valid day!`);
 
-// const day = 'thursday';
+const day = 'saturday';
 
 if(day === 'monday') {
     console.log(`Plan course structure`);
     console.log(`Go to coding meetup`);
 } else if (day === 'tuesday') {
     console.log(`Prepare theory videos`);
-} else if (day === ('wednesday' || 'thursday')) {
+} else if (day === 'wednesday' ||  day === 'thursday') {
     console.log(`Write code examples`);
-} else if (day === friday) {
+} else if (day === 'friday') {
     console.log(`Record Videos`);
-} else if (day === ('saturday' || 'sunday')) {
+} else if (day === 'saturday' ||  day === 'sunday') {
     console.log(`Enjoy the weekend`);
 } else {
     console.log(`Not a valid day!`);
