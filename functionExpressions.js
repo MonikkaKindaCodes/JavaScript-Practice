88cdccb45eebfb3c28b3eaf9e2e7631e62ae2d29
Monikka Edgeston <59119237+MonikkaKindaCodes@users.noreply.github.com>
2020-11-05 10:15:11 -0600

    Add files via upload

---
+++ functionExpressions.js
@@ -0,0 +1,15 @@
+function calcAge1(birthYear) { //Function Expressions have a name
+    return 2020 - birthYear; //You can call the function using the function name
+}
+
+const age1 = calcAge1(1993);
+console.log(`I am ${age1}`); // Function Declaration
+
+//Function Expression
+//varialble will be the function
+ const calcAge2 = function (birthYear) { //Function doesn't have a name/ Anonymous function
+    return 2020 - birthYear;            //This is an expression => Expressions have value
+}
+
+const age2 = calcAge2(1993);
+console.log(age2, age1);
